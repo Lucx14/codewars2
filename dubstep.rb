ba390df34f90d68609b9e9da23d8d03978bbd8aa
Lucien Najev <luciennajev@hotmail.com>
2018-06-25 12:25:04 +0100

    Dubstep exercise added

---
+++ dubstep.rb
@@ -0,0 +1,16 @@
+def song_decoder(song)
+
+
+print new_arr =  song.gsub("WUB", " ").strip.squeeze(" ")
+
+
+
+
+end
+
+
+
+#song_decoder("AWUBBWUBC")        #, "A B C","WUB should be replaced by 1 space");
+#song_decoder("AWUBWUBWUBBWUBWUBWUBC")        #, "A B C","multiples WUB should be replaced by only 1 space");
+#song_decoder("WUBAWUBBWUBCWUB")        #, "A B C", "heading or trailing spaces should be removed");
+song_decoder("WUBWUBHIWUBWUBHOWWUBAREWUBWUBYOUWUB")

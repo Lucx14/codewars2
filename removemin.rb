557aa53fbd257d3781b5865059634fb3f97f7933
Lucien Najev <luciennajev@hotmail.com>
2018-06-30 11:35:02 +0100

    remove min in array added

---
+++ removemin.rb
@@ -0,0 +1,59 @@
+def remove_smallest(numbers)
+
+
+ numbers == [] ? numbers : numbers.delete_at(numbers.index(numbers.min))
+
+=begin
+  if numbers == []
+    numbers
+
+  else
+
+numbers.delete_at(numbers.index(numbers.min))
+  end
+
+=end
+
+print numbers
+
+end
+
+
+remove_smallest([1, 2, 3, 4, 5])         #, [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]")
+remove_smallest([5, 3, 2, 1, 4])         #, [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]")
+remove_smallest([2, 2, 1, 2, 1])         #, [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]")
+remove_smallest([])         #, [], "Wrong result for []")
+
+
+
+=begin
+
+describe "remove_smallest" do
+  it "works for the examples" do
+    Test.assert_equals(remove_smallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]")
+    Test.assert_equals(remove_smallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]")
+    Test.assert_equals(remove_smallest([2, 2, 1, 2, 1]), [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]")
+    Test.assert_equals(remove_smallest([]), [], "Wrong result for []")
+  end
+
+  def randarray()
+    (1..rand(1..10)).map{rand(1..400)}
+  end
+
+  it "returns [] if array has only one element" do
+    10.times do
+      x = rand(1..400)
+      Test.assert_equals(remove_smallest([x]), [], "Wrong result for [#{x}]")
+    end
+  end
+
+  it "returns an array that misses only one element" do
+    10.times do
+      arr = randarray()
+      Test.assert_equals(remove_smallest(arr.dup).length, arr.length - 1, "Wrong sized result for #{arr}")
+    end
+  end
+end
+
+
+=end
